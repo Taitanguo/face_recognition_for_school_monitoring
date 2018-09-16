53d0025699d3a9e7f3f688d89d122b9ec0a2eeb7
sguo <uvasiyuan@gmail.com>
2018-09-16 10:16:28 -0700

    Add facial feature abstraction

--- face_recognition_start/urls.py
+++ face_recognition_start/urls.py
@@ -1,4 +1,4 @@
-"""face_recognition URL Configuration
+"""face_recognition_for_school_monitoring URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
